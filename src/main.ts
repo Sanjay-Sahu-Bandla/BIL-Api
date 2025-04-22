027c079cdb9ef9f486bd48ac8c3d1b50cc4c1e7e
Sanjay-Sahu-Bandla <sanjaysahubandla@gmail.com>
2025-04-22 17:24:51 +0530

    fix: proxy issue

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,12 @@ import rateLimit from 'express-rate-limit';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Apply rate limiting middleware
+  app.getHttpAdapter().getInstance().set('trust proxy', true);
+  // Apply rate limiting middleware
   app.use(
     rateLimit({
-      windowMs: 1 * 60 * 1000, // 1 minute
-      max: 60, // Limit each IP to 60 requests per windowMs
+      windowMs: 15 * 60 * 1000, // 15 minutes
+      max: 1000, // Limit each IP to 1000 requests per windowMs
       message: {
         message: 'Too many requests from your IP, please try again later.',
         statusCode: 429,
