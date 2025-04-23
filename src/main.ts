0305b21796c8cbc96e5593ee081b26d7bd845f94
Sanjay-Sahu-Bandla <sanjaysahubandla@gmail.com>
2025-04-23 09:38:35 +0530

    fix: trust proxy issue

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import rateLimit from 'express-rate-limit';
+// import rateLimit from 'express-rate-limit';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   // Apply rate limiting middleware
-  app.getHttpAdapter().getInstance().set('trust proxy', true);
-  // Apply rate limiting middleware
-  app.use(
-    rateLimit({
-      windowMs: 15 * 60 * 1000, // 15 minutes
-      max: 1000, // Limit each IP to 1000 requests per windowMs
-      message: {
-        message: 'Too many requests from your IP, please try again later.',
-        statusCode: 429,
-      },
-    }),
-  );
+  // app.getHttpAdapter().getInstance().set('trust proxy', true);
+  // // Apply rate limiting middleware
+  // app.use(
+  //   rateLimit({
+  //     windowMs: 15 * 60 * 1000, // 15 minutes
+  //     max: 1000, // Limit each IP to 1000 requests per windowMs
+  //     message: {
+  //       message: 'Too many requests from your IP, please try again later.',
+  //       statusCode: 429,
+  //     },
+  //   }),
+  // );
   app.useGlobalPipes(new ValidationPipe({}));
   app.setGlobalPrefix('api');
   app.enableCors();
